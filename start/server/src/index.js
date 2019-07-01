9950b98dbd222192a2ea20c710998379a7fea3d1
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-07-01 09:16:13 -0700

    Add data sources to apollo server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,21 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils'); // set up database
 
-const server = new ApolloServer({ typeDefs });
+// set up data sources
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
+
+// creates db
+const store = createStore()
+
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
