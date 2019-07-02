441db525439d98023a7bbd0b0d59742a27e144a4
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-07-02 10:00:56 -0700

    Add virtual fields to server data

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+import { GET_CART_ITEMS } from './pages/cart';
 
 // To build a client schema, we extend the types of our server schema and wrap it with the gql function. Using the extend keyword allows us to combine both schemas inside developer tooling like Apollo VSCode and Apollo DevTools.
 
@@ -17,4 +18,18 @@ export const typeDefs = gql`
     }
 `
 
-export const resolvers = {}
+export const schema = gql`
+    extend type Launch {
+        isInCart: Boolean!
+}
+`
+
+// The important thing to note is that the resolver API on the client is the same as the resolver API on the server.
+export const resolvers = {
+    Launch: {
+        isInCart: (launch, _, { cache }) => {
+            const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS })
+            return cartItems.includes(launch.id)
+        }
+    }
+}
