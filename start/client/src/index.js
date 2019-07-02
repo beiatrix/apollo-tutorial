5058adbecabe58bb759614872df03a692e9a5568
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-07-02 09:44:37 -0700

    Initialize the store and add default state to apollo cache

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -5,6 +5,7 @@ import { HttpLink } from 'apollo-link-http';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -19,14 +20,17 @@ const client = new ApolloClient({
     headers: {
       authorization: localStorage.getItem('token')
     }
-  })
+  }),
+  typeDefs,
+  resolvers
 })
 
+// adds default state to the Apollo cache
 cache.writeData({
   data: {
     isLoggedIn: !!localStorage.getItem('token'),
-    cartItems: []
-  }
+    cartItems: [],
+  },
 })
 
 // To connect Apollo Client to React, we will wrap our app in the ApolloProvider component exported from the react-apollo package and pass our client to the client prop. The  ApolloProvider component is similar to React’s context provider. It wraps your React app and places the client on the context, which allows you to access it from anywhere in your component tree.
